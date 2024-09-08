4ff40a22d639fa308001dc2bec87eeb2ea2a25de
Jean Berin <51207250+PiLord1@users.noreply.github.com>
2024-09-08 12:35:08 +0800

    fixed selectCategory function

--- index.js
+++ index.js
@@ -1,71 +1,48 @@
 const taskForm = document.getElementById('enter-new-task');
 const inputNewTask = document.getElementById('input-task');
-const addTaskBtn = document.getElementById('submit');
 const taskList = document.getElementById('tasks');
-let storeToDoList = [];
 let list = [];
 let colorList = [];
 let categoryList = ['pink', 'lightblue', 'dandelion'];
 
 function addTask() {
     if (inputNewTask.value === '') {
-        alert('Please enter task.');
+        alert('Please enter a task.');
     } else {
         list.push(inputNewTask.value);
-        // selectCategory();
+        selectCategory(); // Assign a random color to the new task
         renderList();
+        saveToDoList(); 
     }
 }
 
 function selectCategory() {
-    let selectedCategory = categoryList[generateRandomNumber(0,2)];
-    colorList.push(selectedCategory);
+    let selectedCategory = categoryList[generateRandomNumber(0, categoryList.length - 1)];
+    colorList.push(selectedCategory); 
     console.log(colorList);
 }
 
 function renderList() {
-    let originalTask;
-    let indexOfOriginalTask;
+    taskList.innerHTML = ''; 
 
-    taskList.innerHTML = '';
-    for(let i = 0; i < list.length; i++) {
+    for (let i = 0; i < list.length; i++) {
         const newTask = document.createElement('div');
         newTask.setAttribute('class', 'task');
-        
-
-
-
-
 
         const categoryBtn = document.createElement('div');
         categoryBtn.setAttribute('class', 'category-btn');
-        // categoryBtn.classList.add(`${colorList[i]}`);
-        // FOR ADDING COLOR PROPERTY OF CATEGORY-BTN
-        categoryBtn.classList.add(`${categoryList[generateRandomNumber(0,2)]}`);
-        
-
-
-
+        categoryBtn.classList.add(colorList[i]); 
 
         const taskContentContainer = document.createElement('div');
         taskContentContainer.setAttribute('class', 'task-content');
+
         const readOnlyTask = document.createElement('input');
         readOnlyTask.setAttribute('class', 'text');
         readOnlyTask.readOnly = true;
         readOnlyTask.type = 'text';
         readOnlyTask.setAttribute('value', list[i]);
 
-
-
-
-
         taskContentContainer.append(categoryBtn);
-
-
-
-
-
-
         taskContentContainer.append(readOnlyTask);
         newTask.append(taskContentContainer);
 
@@ -73,77 +50,59 @@ function renderList() {
         btnContainer.setAttribute('class', 'btn-container');
 
         const editBtn = document.createElement('button');
-        editBtn.addEventListener('click', function() {
-            if (editBtn.innerHTML === '<i class="fa-solid fa-pen"></i>') {
-                readOnlyTask.readOnly = false
+        editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+        editBtn.addEventListener('click', function () {
+            if (readOnlyTask.readOnly) {
+                readOnlyTask.readOnly = false;
                 editBtn.innerHTML = '<i class="fa-solid fa-floppy-disk"></i>';
-                originalTask = readOnlyTask.value;
-                indexOfOriginalTask = list.indexOf(originalTask);
                 readOnlyTask.focus();
-
-        
             } else {
                 readOnlyTask.readOnly = true;
                 editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
-                list[indexOfOriginalTask] = readOnlyTask.value;
+                list[i] = readOnlyTask.value; 
                 saveToDoList();
-                console.log(list);
             }
-        })
-        editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+        });
 
         const deleteBtn = document.createElement('button');
-        deleteBtn.addEventListener('click', function() {
-            let childrenArray = Array.from(taskList.children);
-            console.log(childrenArray);
-            list.splice(childrenArray.indexOf(taskList.removeChild(newTask)), 1);
-
-
-
-            // colorList.splice(childrenArray.indexOf(taskList.removeChild(newTask)), 1)
-
-
-
-            saveToDoList();
-            console.log(list);
-        })
         deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+        deleteBtn.addEventListener('click', function () {
+            list.splice(i, 1); 
+            colorList.splice(i, 1); 
+            saveToDoList(); 
+            renderList(); 
+        });
 
         btnContainer.append(editBtn);
         btnContainer.append(deleteBtn);
         newTask.append(btnContainer);
         taskList.append(newTask);
-        inputNewTask.value = '';
     }
+
+    inputNewTask.value = ''; 
 }
 
-taskForm.addEventListener('submit', function(e) {
+taskForm.addEventListener('submit', function (e) {
     e.preventDefault();
     addTask();
-    saveToDoList();
-})
+});
 
 function saveToDoList() {
     localStorage.setItem('savedToDoList', JSON.stringify(list));
-    localStorage.setItem('colorList', JSON.stringify(colorList))
-    list = JSON.parse(localStorage.getItem('savedToDoList'));
-    colorList = JSON.parse(localStorage.getItem('colorList'));
+    localStorage.setItem('colorList', JSON.stringify(colorList));
 }
 
 function getToDoList() {
-    list = JSON.parse(localStorage.getItem('savedToDoList'));
-    colorList = JSON.parse(localStorage.getItem('colorList'));
+    list = JSON.parse(localStorage.getItem('savedToDoList')) || [];
+    colorList = JSON.parse(localStorage.getItem('colorList')) || [];
 }
 
 function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// if (localStorage.getItem('savedToDoList')) {
-//     getToDoList();
-//     renderList();
-// }
-if (localStorage.getItem('savedToDoList') !== null && localStorage.getItem('colorList')) {
+// Load the task list and colors from localStorage if they exist
+if (localStorage.getItem('savedToDoList') !== null && localStorage.getItem('colorList') !== null) {
     getToDoList();
     renderList();
 }
